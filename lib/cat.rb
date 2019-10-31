2630c1b401dc85b2f51b51297f91bcb76618c052
Flatiron School <stvik2@gmail.com>
2019-10-31 15:18:01 -0400

    Cats and Dogs

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,30 @@
 class Cat
-  # code goes here
+
+  @@all = []
+
+  attr_accessor :mood, :owner
+  attr_reader :name
+  #initialized with name (read only) and owner object
+  #initial mood = nervous (read and write)
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+
+    @@all << self
+  end
+  #new_owner: method to change owner 
+
+  def new_owner(new_name)
+    @owner = new_name
+  end
+
+  def self.all
+    @@all
+  end
+
+  #Class method to store all cats
+
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,88 @@
 class Owner
-  # code goes here
+  # initalize with name (read only)
+  #initialize with species set to human (read only)
+  attr_reader :name, :species
+ @@all =[]
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+
+
+  #buy cat method to buy cat. Takes in argument of name
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+  #def buy_cat(name)
+    #spot = Cat.new(name, self)
+   #buy dog method to buy dog. Takes in argument of name
+    def buy_dog(name)
+    dog = Dog.new(name, self)
+  end
+
+  #sell_dog/sell_cat
+  def sell_pets
+    self.pets.each do |pet|
+      pet.mood = "nervous"
+      pet.owner = nil
+    end
+
+  end
+
+  #walk pet method. Changes pet mood to happy.
+  def walk_dogs
+
+    self.dogs.each do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+  #feed pet method. Change pet mood to happy.
+  def feed_cats
+    self.cats.each do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  #method to say species
+  def say_species
+    return "I am a #{@species}."
+  end
+
+  #class method to return all instances of Owner created
+  def self.all
+    @@all
+  end
+  #method to count number of owners
+
+  def self.count
+    @@all.length
+  end
+
+  #removes all owners created
+
+  def self.reset_all
+    @@all = []
+  end
+
+  def cats
+    Cat.all.select do |cat|
+      cat.owner == self
+    end
+  end
+
+  def dogs
+    Dog.all.select do |dog|
+      dog.owner == self
+    end
+  end
+
+  def pets
+    self.cats + self.dogs
+  end
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end 
 end
