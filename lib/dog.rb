5087a59cc6ca7d38bce04200c8f9a17b92b67d0b
remiodufuye <remi.odufuye@gmail.com>
2019-10-31 14:21:43 -0400

    Create Owner Class

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
+
 class Dog
+      
+     attr_accessor :owner 
+     attr_reader :name
+
+      def initialize(name=nil,owner=nil)
+      @name = name 
+      @owner = owner
+      end 
   # code goes here
+
+    
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,31 @@
 class Owner
+
+  @@all = [] 
+   
+   attr_reader :name , :species
+
+
+    def initialize (name)
+      @name = name 
+      @species = "human" 
+      @@all << self
+    end 
+
+    def say_species 
+      "I am a #{@species}."
+    end 
+
+    # Method below returns all our instances of the Owner Class 
+     def self.all 
+      return @@all 
+     end
+
+      def self.count
+         @@all.length
+      end 
+
+      def self.reset_all 
+        @@all.clear
+      end 
   # code goes here
-end
+end 
